982c17afbac094b114c47f3416a64b6090c3b421
MelissaHoffman09 <melisssahoffman09@gmail.com>
2021-12-28 23:48:19 -0800

    #

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
-let inputBtn = document.getElementById("input-btn") 
+let myLeads = ["www.awesome.com", "www.epic.com", "www.great.com"]
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
-    console.log("Button clicked from addEventListener")
+    myLeads.push(inputEl.value)
+    renderLeads()
+// clear box after clicked
+    inputEl.value = ""
+    // Save the myLeads array to localStorage 
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    renderLeads()
+    
+    // To verify that it works:
+    console.log( localStorage.getItem("myLeads") )
 })
+
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += 
+        `<li>
+            <a target='_blank' href='${myLeads[i]}'>
+                ${myLeads[i]}
+            </a>
+        </li>`
+    }
+
+    ulEl.innerHTML = listItems
+}
